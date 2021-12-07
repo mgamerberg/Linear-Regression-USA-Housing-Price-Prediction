994eb74ea14c67d434d98abd9675e9df7db9e5e2
mgamerberg <94943291+mgamerberg@users.noreply.github.com>
2021-12-07 09:49:13 -0800

    Add files via upload

--- BSDS Class Final practice.R
+++ BSDS Class Final practice.R
@@ -10,6 +10,23 @@ head(USAHousing)
 
 ggplot(USAHousing, aes(x=Avg..Area.Income , y=Price)) + geom_point()
 cor(USAHousing$Avg..Area.Income , USAHousing$Price)
+
+ggplot(USAHousing, aes(x=Avg..Area.House.Age , y=Price)) + geom_point()
+cor(USAHousing$Avg..Area.House.Age , USAHousing$Price)
+
+ggplot(USAHousing, aes(x=Avg..Area.Number.of.Rooms , y=Price)) + geom_point()
+cor(USAHousing$Avg..Area.Number.of.Rooms , USAHousing$Price)
+
+ggplot(USAHousing, aes(x=Avg..Area.Number.of.Bedrooms , y=Price)) + geom_point()
+cor(USAHousing$Avg..Area.Number.of.Bedrooms , USAHousing$Price)
+
+ggplot(USAHousing, aes(x=Area.Population , y=Price)) + geom_point()
+cor(USAHousing$Area.Population , USAHousing$Price)
+
+ggplot(USAHousing, aes(x=Address , y=State)) + geom_point()
+cor(USAHousing$Address , USAHousing$Price)
+
 #add a correlation line to the graph
 #compare all of the correlation values to see which one is the strongest indicator of the
 #house price
+
